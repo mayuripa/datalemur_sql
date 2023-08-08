6f6abc7f3199f0e7ccbe528db96e6bf224520686
Mayuri Awati <42701751+mayuripa@users.noreply.github.com>
2023-08-08 16:30:00 +0530

    Delete SQL questions directory

--- SQL questions/Easy/Histogram of Tweets [Twitter SQL Interview Question].sql
+++
@@ -1,9 +0,0 @@
---https://datalemur.com/questions/sql-histogram-tweets
-with user_tweet AS (
-SELECT user_id,count(*) AS no_tweet FROM tweets
-where tweet_date BETWEEN '01-01-2022' and '12-31-2022' 
-GROUP BY user_id)
-
-SELECT no_tweet AS tweet_bucket,COUNT(*) AS num_of_user
-FROM user_tweet
-GROUP BY no_tweet
--- SQL questions/Easy/Page With No Likes [Facebook SQL Interview Question].sql
+++
@@ -1,6 +0,0 @@
-SELECT page_id
-FROM pages
-WHERE page_id NOT IN (
-SELECT DISTINCT page_id 
-FROM page_likes ) 
-ORDER BY page_id;
